c872fcd96a05702473e19723b0dd33ddced06d6d
anaomart <anaomart@gmail.com>
2023-05-02 13:49:00 +0300

    fix cors

--- index.js
+++ index.js
@@ -13,9 +13,19 @@ const globalMiddleWareError = require('./util/globalMiddleWareError');
 
 // middle wares
 app.use(cors())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://pixld.agency');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.pixld.agency');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.json())
 app.use(express.static('uploads'));
 // routes
+console.log("userInfo ")
+
 app.use('/user', userRoute)
 app.use('/pin', verify, pinRoute)
 app.use('/comment', verify, commentRoute);
